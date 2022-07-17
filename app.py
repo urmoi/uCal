bde6fd2039d75efbc8f3c4bdbbe342fb41f17e77
urmoi <urmoi@posteo.com>
2022-07-17 16:38:37 +0200

    init repo

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask, redirect, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+  app.run()
