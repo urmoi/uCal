d19a77a39db645d0701ac46e2047ca9dfd7f6ba8
urmoi <urmoi@posteo.com>
2022-07-17 17:05:13 +0200

    some changes

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
-
-if __name__ == "__main__":
-  app.run()
