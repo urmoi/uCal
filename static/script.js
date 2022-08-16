54e04652294c7c6d78754375668b771c1433e0ff
urmoi <urmoi@posteo.com>
2022-08-16 12:27:36 +0200

    javascript load fox

--- static/script.js
+++ static/script.js
@@ -1,3 +1,29 @@
+let data = {};
+
+let date = new Date();
+Object.defineProperty(data, "date", {
+    set(newDate) {
+        date = newDate;
+        updateCalendar();
+    },
+    get() { return date; }
+});
+
+let library = {};
+Object.defineProperty(data, "library", {
+    set(newLibrary) {
+        if (Object.keys(library).length === 0) {
+            toggleNavOnInput();
+            activateNode("calendar-shortcut");
+        };
+        library = newLibrary;
+        updateShortcutSelection();
+        updateShortcutCards();
+        updateLocationList();
+    },
+    get() { return library; }
+});
+
 /* https://github.com/nwcell/FileSaver.js */
 /*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js */
 var saveAs=saveAs||function(e){"use strict";if(typeof e==="undefined"||typeof navigator!=="undefined"&&/MSIE [1-9]\./.test(navigator.userAgent)){return}var t=e.document,n=function(){return e.URL||e.webkitURL||e},r=t.createElementNS("http://www.w3.org/1999/xhtml","a"),o="download"in r,a=function(e){var t=new MouseEvent("click");e.dispatchEvent(t)},i=/constructor/i.test(e.HTMLElement)||e.safari,f=/CriOS\/[\d]+/.test(navigator.userAgent),u=function(t){(e.setImmediate||e.setTimeout)(function(){throw t},0)},s="application/octet-stream",d=1e3*40,c=function(e){var t=function(){if(typeof e==="string"){n().revokeObjectURL(e)}else{e.remove()}};setTimeout(t,d)},l=function(e,t,n){t=[].concat(t);var r=t.length;while(r--){var o=e["on"+t[r]];if(typeof o==="function"){try{o.call(e,n||e)}catch(a){u(a)}}}},p=function(e){if(/^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(e.type)){return new Blob([String.fromCharCode(65279),e],{type:e.type})}return e},v=function(t,u,d){if(!d){t=p(t)}var v=this,w=t.type,m=w===s,y,h=function(){l(v,"writestart progress write writeend".split(" "))},S=function(){if((f||m&&i)&&e.FileReader){var r=new FileReader;r.onloadend=function(){var t=f?r.result:r.result.replace(/^data:[^;]*;/,"data:attachment/file;");var n=e.open(t,"_blank");if(!n)e.location.href=t;t=undefined;v.readyState=v.DONE;h()};r.readAsDataURL(t);v.readyState=v.INIT;return}if(!y){y=n().createObjectURL(t)}if(m){e.location.href=y}else{var o=e.open(y,"_blank");if(!o){e.location.href=y}}v.readyState=v.DONE;h();c(y)};v.readyState=v.INIT;if(o){y=n().createObjectURL(t);setTimeout(function(){r.href=y;r.download=u;a(r);h();c(y);v.readyState=v.DONE});return}S()},w=v.prototype,m=function(e,t,n){return new v(e,t||e.name||"download",n)};if(typeof navigator!=="undefined"&&navigator.msSaveOrOpenBlob){return function(e,t,n){t=t||e.name||"download";if(!n){e=p(e)}return navigator.msSaveOrOpenBlob(e,t)}}w.abort=function(){};w.readyState=w.INIT=0;w.WRITING=1;w.DONE=2;w.error=w.onwritestart=w.onprogress=w.onwrite=w.onabort=w.onerror=w.onwriteend=null;return m}(typeof self!=="undefined"&&self||typeof window!=="undefined"&&window||this.content);if(typeof module!=="undefined"&&module.exports){module.exports.saveAs=saveAs}else if(typeof define!=="undefined"&&define!==null&&define.amd!==null){define("FileSaver.js",function(){return saveAs})};
@@ -86,6 +112,52 @@ function addShortcut () {
 
     resetLibraryForm(form);
 }
+
+function updateCalendar () {
+    updateDateInput();
+
+    let [month, year] = [data.date.getMonth(), data.date.getFullYear()];
+
+    var dayTemp = document.querySelector("template#template-calendar-day");
+
+    var container = document.getElementById("calendar-input");
+
+    var new_container = document.createElement("div");
+
+    let daysInWeek = 7;
+    let daysInMonth = new Date(year, month+1, 0).getDate();
+    let firstDay = new Date(year, month, 1).getDay() || 7;
+    let weeksInMonth = (firstDay + daysInMonth - 1) / daysInWeek;
+
+    for (let i = 0; i < weeksInMonth; i++) {
+        let week = document.createElement("div");
+        week.classList.add("row", "mx-auto", "row-cols-7");
+
+        for (let j = 1; j <= daysInWeek; j++) {
+            let numberDay = i * daysInWeek + j;
+            let calendarDay = numberDay - firstDay + 1;
+            let isDay = calendarDay >= 1 && calendarDay <= daysInMonth;
+            let node = document.importNode(dayTemp.content, true);
+            if (isDay) {
+                node.querySelector("button").addEventListener("click", dayInput);
+                node.querySelector("label").innerHTML = (calendarDay) + "." + (month+1) + ".";
+                node.querySelector("label").setAttribute("for", "calendar-day-"+calendarDay+"-"+(month+1)+"-"+year);
+                node.querySelector("textarea").id = "calendar-day-"+calendarDay+"-"+(month+1)+"-"+year;
+            } else {
+                node.children[0].disabled = true;
+                node.children[0].innerHTML = "";
+            };
+            week.appendChild(node);
+        };
+        new_container.appendChild(week);
+    };
+    container.replaceChildren(...new_container.childNodes);
+}
+
+function updateDateInput () {
+    document.getElementById("calendar-date-input").value = getFormattedStringFromDate(data.date);
+}
+
 
 function toggleNavInit (e) {
     let showMode = e.currentTarget.getAttribute("data-nav");
@@ -352,6 +424,11 @@ function getDateFromString () {
 function getStringFromDate () {
     return ("00"+(date.getMonth()+1)).slice(-2)+" / "+date.getFullYear();
 }
+
+function getFormattedStringFromDate (date) {
+    return date.toLocaleString("en-US", { month: "long", year: "numeric" });
+}
+
 
 function getTimeString (y, m, d, t) {
     y = parseInt(y); m = parseInt(m); d = parseInt(d);
