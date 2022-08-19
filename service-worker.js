2f947be157953b202f67aecca695feceed1416bf
urmoi <urmoi@posteo.com>
2022-08-20 00:07:22 +0200

    delete service worker

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', () => console.log('[ServiceWorker] Install'));
-self.addEventListener('activate', () => console.log('[ServiceWorker] Activate'));
-self.addEventListener('fetch', () => console.log('[Service Worker] Fetch'));
